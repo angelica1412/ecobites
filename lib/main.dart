822c07df0569793cb1e5026829067d66a111f21e
dewanugrah <dewaanugrahaljasputra.2003@gmail.com>
2024-05-13 16:27:37 +0800

    ganti ukuran foto profile sama tambah const, sama hapus import yang unused

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'dart:async';
-import 'package:ecobites/Store.dart';
-import 'package:ecobites/UploadBarang.dart';
-import 'package:ecobites/profile.dart';
-import 'package:ecobites/tau.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'onboardingscreen.dart';
